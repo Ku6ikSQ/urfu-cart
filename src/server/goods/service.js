a2f7d5eb91d586a7387a33b31c4b66eabf8388f3
Tim <tikhonow66@gmail.com>
2024-11-09 15:45:50 +0500

    upd: refactor code and docs

--- src/server/goods/service.js
+++ src/server/goods/service.js
@@ -2,38 +2,39 @@ import db from "../db/db.js";
 
 export default class GoodsService {
     static async getGoodsById(id) {
-        const result = await db.query("SELECT * FROM goods WHERE id = $1", [id]);
-        if (!result.rows[0]) throw new Error("Goods not found");
-        return result.rows[0];
+        const result = await db.query("SELECT * FROM goods WHERE id = $1", [id])
+        if (!result.rows[0]) 
+            throw new Error("Goods not found")
+        return result.rows[0]
     }
 
-    // TODO: probably, it will be rewrited
+    // TODO: probably, it will be rewrited (later)
     static async getAllGoods() {
-        const result = await db.query("SELECT * FROM goods");
-        return result.rows;
+        const result = await db.query("SELECT * FROM goods")
+        return result.rows
     }
 
     static async createGoods(name, description, price, categoryId, photos) {
-        // TODO: in fact, we need to save the photos on the storage and after get photo names and save it in database.
         const result = await db.query(
             "INSERT INTO goods (name, description, price, category_id, photos) VALUES ($1, $2, $3, $4, $5) RETURNING *",
             [name, description, price, categoryId, photos]
-        );
-        return result.rows[0];
+        )
+        return result.rows[0]
     }
 
     static async updateGoods(id, name, description, price, categoryId, photos) {
-        // TODO: in fact, we need to save the photos on the storage and after get photo names and save it in database.
         const result = await db.query(
             "UPDATE goods SET name = $1, description = $2, price = $3, category_id = $4, photos = $5 WHERE id = $6 RETURNING *",
             [name, description, price, categoryId, photos, id]
-        );
-        if (!result.rows[0]) throw new Error("Goods not found");
-        return result.rows[0];
+        )
+        if (!result.rows[0]) 
+            throw new Error("Goods not found")
+        return result.rows[0]
     }
 
     static async deleteGoods(id) {
-        const result = await db.query("DELETE FROM goods WHERE id = $1 RETURNING *", [id]);
-        if (!result.rows[0]) throw new Error("Goods not found");
+        const result = await db.query("DELETE FROM goods WHERE id = $1 RETURNING *", [id])
+        if (!result.rows[0]) 
+            throw new Error("Goods not found")
     }
 }
