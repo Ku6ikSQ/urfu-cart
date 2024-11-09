8edac860a340d5942152a237c888beadd473cb0b
Tim <tikhonow66@gmail.com>
2024-11-09 17:11:58 +0500

    upd: database has been deployed

--- src/server/db/db.js
+++ src/server/db/db.js
@@ -1,11 +1,13 @@
 import pg from 'pg'
 
+dotenv.config();
+
 const db = new pg.Pool({
-    user: 'postgres',
-    password: 'root',
-    port: 5432,
-    host: 'localhost',
-    database: 'urfucart',
-})
+    user: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD || 'root',
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 5432,
+    database: process.env.DB_DATABASE || 'urfucart',
+});
 
 export default db
