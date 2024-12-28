625e8a91beef7dc5a61190819c09f90eeadbef68
Tim <tikhonow66@gmail.com>
2024-12-28 19:27:03 +0500

    refactor: cart unit

--- src/server/cart/router.js
+++ src/server/cart/router.js
@@ -3,10 +3,11 @@ import CartController from "./controller.js"
 
 const CartRouter = Router()
 
-// CRUD для корзины
-CartRouter.get("/api/cart/:userId", CartController.getCart)
-CartRouter.post("/api/cart/:userId", CartController.addItem)
-CartRouter.patch("/api/cart/:userId/:goodsId", CartController.updateItem)
-CartRouter.delete("/api/cart/:userId/:goodsId", CartController.removeItem)
+CartRouter.get("/api/cart/user/:userId", CartController.getCarts)
+CartRouter.get("/api/cart/:cartId", CartController.getCartById)
+CartRouter.post("/api/cart", CartController.createCart)
+CartRouter.patch("/api/cart/:cartId/:goodsId", CartController.updateItem)
+CartRouter.delete("/api/cart/:cartId/:goodsId", CartController.removeItem)
+CartRouter.post("/api/cart/:cartId/items", CartController.addItem)
 
 export default CartRouter
--- src/server/cart/service.js
+++ src/server/cart/service.js
@@ -1,42 +1,31 @@
 import db from "../db/db.js"
 
 export default class CartService {
-    static async getCartByUserId(userId) {
-        const cartResult = await db.query(
+    static async getCartsByUserId(userId) {
+        const cartsResult = await db.query(
             "SELECT * FROM Cart WHERE user_id = $1",
             [userId]
         )
-        const cart = cartResult.rows[0]
-
-        if (!cart) {
-            return { user_id: userId, items: [] }
-        }
-
-        const itemsResult = await db.query(
-            "SELECT * FROM CartItems WHERE cart_id = $1",
-            [cart.id]
+        const carts = cartsResult.rows
+        const itemsPromises = carts.map((cart) =>
+            db.query("SELECT * FROM CartItems WHERE cart_id = $1", [cart.id])
         )
-        return { user_id: userId, items: itemsResult.rows }
+        const itemsResults = await Promise.all(itemsPromises)
+
+        return carts.map((cart, index) => ({
+            cart_id: cart.id,
+            user_id: userId,
+            main: cart.main,
+            items: itemsResults[index].rows,
+        }))
     }
 
-    static async addItemToCart(userId, goodsId, count, selected) {
-        let cart = await db.query("SELECT * FROM Cart WHERE user_id = $1", [
-            userId,
-        ])
-        if (cart.rows.length === 0) {
-            const newCart = await db.query(
-                "INSERT INTO Cart (user_id) VALUES ($1) RETURNING *",
-                [userId]
-            )
-            cart = newCart.rows[0]
-        } else {
-            cart = cart.rows[0]
-        }
-
+    static async addItemToCart(cartId, goodsId, count, selected) {
         const existingItemIndex = await db.query(
             "SELECT * FROM CartItems WHERE cart_id = $1 AND goods_id = $2",
-            [cart.id, goodsId]
+            [cartId, goodsId]
         )
+
         if (existingItemIndex.rows.length > 0) {
             const item = existingItemIndex.rows[0]
             await db.query(
@@ -46,22 +35,16 @@ export default class CartService {
         } else {
             await db.query(
                 "INSERT INTO CartItems (cart_id, goods_id, count, selected) VALUES ($1, $2, $3, $4)",
-                [cart.id, goodsId, count, selected]
+                [cartId, goodsId, count, selected]
             )
         }
-
-        return this.getCartByUserId(userId)
+        return this.getCartById(cartId)
     }
 
-    static async updateItemInCart(userId, goodsId, count, selected) {
-        const cart = await db.query("SELECT * FROM Cart WHERE user_id = $1", [
-            userId,
-        ])
-        if (cart.rows.length === 0) throw new Error("Cart not found")
-
+    static async updateItemInCart(cartId, goodsId, count, selected) {
         const item = await db.query(
             "SELECT * FROM CartItems WHERE cart_id = $1 AND goods_id = $2",
-            [cart.rows[0].id, goodsId]
+            [cartId, goodsId]
         )
         if (item.rows.length === 0) throw new Error("Item not found in cart")
 
@@ -69,19 +52,43 @@ export default class CartService {
             "UPDATE CartItems SET count = $1, selected = $2 WHERE id = $3",
             [count, selected, item.rows[0].id]
         )
-        return this.getCartByUserId(userId)
+        return this.getCartById(cartId)
     }
 
-    static async removeItemFromCart(userId, goodsId) {
-        const cart = await db.query("SELECT * FROM Cart WHERE user_id = $1", [
-            userId,
-        ])
-        if (cart.rows.length === 0) throw new Error("Cart not found")
-
+    static async removeItemFromCart(cartId, goodsId) {
         await db.query(
             "DELETE FROM CartItems WHERE cart_id = $1 AND goods_id = $2",
-            [cart.rows[0].id, goodsId]
+            [cartId, goodsId]
         )
-        return this.getCartByUserId(userId)
+        return this.getCartById(cartId)
+    }
+
+    static async getCartById(cartId) {
+        const cartResult = await db.query("SELECT * FROM Cart WHERE id = $1", [
+            cartId,
+        ])
+
+        if (cartResult.rows.length === 0) throw new Error("Cart not found")
+
+        const cart = cartResult.rows[0]
+        const itemsResult = await db.query(
+            "SELECT * FROM CartItems WHERE cart_id = $1",
+            [cart.id]
+        )
+
+        return {
+            cart_id: cart.id,
+            user_id: cart.user_id,
+            main: cart.main,
+            items: itemsResult.rows,
+        }
+    }
+
+    static async createCart(userId) {
+        const result = await db.query(
+            "INSERT INTO Cart (user_id, main) VALUES ($1, $2) RETURNING id",
+            [userId, false]
+        )
+        return this.getCartById(result.rows[0].id)
     }
 }
